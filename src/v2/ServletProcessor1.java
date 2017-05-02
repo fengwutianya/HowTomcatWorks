0c913a0b1b8919f4d216648185c1543badbfe350
fengwutianya <949979029@qq.com>
2017-05-02 23:45:16 +0800

    URLClassLoader

--- src/v2/ServletProcessor1.java
+++ src/v2/ServletProcessor1.java
@@ -1,6 +1,11 @@
 package v2;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLClassLoader;
+import java.net.URLStreamHandler;
 
 /**
  * Created by xuan on 2017/5/2 0002.
@@ -11,6 +16,16 @@ public class ServletProcessor1 {
         String servletName = uri.substring(uri.lastIndexOf("/") + 1);
         URLClassLoader loader = null;
 
-        //todo
+        try {
+            URL[] urls = new URL[1];
+            URLStreamHandler streamHandler = null;
+            File classpath = new File(Constants.WEB_ROOT);
+            String repository = (new URL("file", null,
+                    classpath.getCanonicalPath() + File.separator)).toString();
+            urls[0] = new URL(null, repository, streamHandler);
+            loader = new URLClassLoader(urls);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
