9db1650fe30d46596973dc7d2c9e84c5cc9d6e36
fengwutianya <949979029@qq.com>
2017-05-04 17:52:36 +0800

    facade

---
+++ src/v2/HttpServer2.java
@@ -0,0 +1,64 @@
+package v2;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by xuan on 2017/5/4 0004.
+ */
+public class HttpServer2 {
+    private static final String SHUTDOWN_COMMAND = "/SHUTDOWN";
+    private boolean shutdown = false;
+
+    public void await() {
+        ServerSocket serverSocket = null;
+        int port = 8080;
+        try {
+            serverSocket = new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        while (!shutdown) {
+            Socket socket = null;
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+
+            try {
+                socket = serverSocket.accept();
+                inputStream = socket.getInputStream();
+                outputStream = socket.getOutputStream();
+
+                Request request = new Request(inputStream);
+                request.parse();
+
+                Response response = new Response(outputStream);
+                response.setRequest(request);
+
+                if (request.getUri().startsWith("/servlet/")) {
+                    ServletProcessor2 processor = new ServletProcessor2();
+                    processor.process(request, response);
+                } else {
+                    StaticResourceProcessor processor = new StaticResourceProcessor();
+                    processor.process(request, response);
+                }
+
+                socket.close();
+                shutdown = request.getUri().equals(SHUTDOWN_COMMAND);
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        HttpServer2 server2 = new HttpServer2();
+        server2.await();
+    }
+}
